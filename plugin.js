04a9b1136fd02067d43d6d5e4e826b48f254ecfb
Tomasz Jakut <vepomoc@gmail.com>
2016-02-04 10:51:15 +0100

    Renamed plugin to "Copy Formatting".

---
+++ plugin.js
@@ -0,0 +1,80 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+( function() {
+	'use strict';
+
+	function convertElementToStyle( element ) {
+		return new CKEDITOR.style( { element: element.getName() } );
+	}
+
+	function getSelectedWordOffset( range ) {
+		var node = range.startContainer,
+			regex = /\b\w+\b/ig,
+			contents = node.getText(),
+			match;
+
+		while( ( match = regex.exec( contents ) ) != null ) {
+			if ( match.index + match[ 0 ].length >= range.startOffset ) {
+				return {
+					start: match.index,
+					end: match.index + match[ 0 ].length
+				}
+			}
+		}
+
+		return null;
+	}
+
+	var commandDefinition = {
+		exec: function( editor ) {
+			var style = convertElementToStyle( editor.elementPath().lastElement ),
+				cmd = this;
+
+			cmd.setState( CKEDITOR.TRISTATE_ON );
+
+			editor.editable().once( 'click', function( evt ) {
+				var range = editor.getSelection().getRanges()[ 0 ];
+
+				if ( !range ) {
+					return;
+				}
+
+				if ( range.collapsed ) {
+					var newRange = editor.createRange(),
+						word = getSelectedWordOffset( range );
+
+					if ( !word ) {
+						return;
+					}
+
+					newRange.setStart( range.startContainer, word.start );
+					newRange.setEnd( range.startContainer, word.end );
+
+					newRange.select();
+				}
+
+				style.apply( editor );
+				cmd.setState( CKEDITOR.TRISTATE_OFF );
+			} );
+		}
+	};
+
+	CKEDITOR.plugins.add( 'copyformatting', {
+		requires: 'contextmenu',
+		lang: 'en',
+		icons: 'copyformatting',
+		hidpi: true,
+		init: function( editor ) {
+			var command = editor.addCommand( 'copyFormatting', commandDefinition );
+
+			editor.ui.addButton( 'copyFormatting', {
+				label: editor.lang.copyformatting.label,
+				command: 'copyFormatting',
+				toolbar: 'basicstyles,90'
+			} );
+		}
+	} );
+} )();
